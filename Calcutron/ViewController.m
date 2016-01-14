c339c4db85f8d65398b9f0bb6f2d016dc7391025
Michael Saltzman <saltma84@suny.oneonta.edu>
2016-01-14 10:21:32 -0800

    Initial Commit

---
+++ Calcutron/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calcutron
+//
+//  Created by Michael Saltzman on 1/14/16.
+//  Copyright © 2016 Michael Saltzman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
